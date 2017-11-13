6db56ddac5834459920c74e30bbd92179c688af2
prashaaant <myinbox.pc@gmail.com>
2017-11-13 17:36:19 +0530

    Initial commit

---
+++ servo_motor.30sept.ino
@@ -0,0 +1,28 @@
+
+// Include the Servo library
+#include <Servo.h> 
+
+// Declare the Servo pin
+int servoPin = 3;
+// Create a servo object
+Servo Servo1;
+
+void setup() {
+  Serial.begin(9600);
+  // We need to attach the servo to the used pin number
+  Servo1.attach(servoPin);
+}
+
+void loop(){
+  Serial.print("inside");
+  // Make servo go to 0 degrees
+  Servo1.write(0);
+  delay(1000);
+  // Make servo go to 90 degrees
+  Servo1.write(90);
+  delay(1000);
+  // Make servo go to 180 degrees
+  Servo1.write(180);
+  delay(1000);
+}
+
